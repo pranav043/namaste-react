b8d91f210903b2a5536e331851583eb8d64e5c96
pranav043 <gupta.pranav043@gmail.com>
2023-12-06 23:10:49 +0530

    first commit

---
+++ 1_First_React_CDN/script.js
@@ -0,0 +1,17 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement(
+  "h1", //element type
+  { id: "heading" }, //attribute
+  "Hello From React!!!" //children
+);
+
+const heading2 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "H1 Hello"),
+    React.createElement("h2", {}, "H2 Hello"),
+  ])
+);
+root.render(heading2);
